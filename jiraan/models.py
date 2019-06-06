5a926cc15bfbe334cd6b903ebc6b36707168e0b7
saudahabib <saudababs00@gmail.com>
2019-06-06 15:12:12 +0300

    order articles by negative id

--- jiraan/models.py
+++ jiraan/models.py
@@ -6,14 +6,29 @@ class Neighborhood(models.Model):
     hood_location = models.CharField(max_length=50)
     hood_count=models.CharField(max_length=50)
 
+
+    def __str__(self):
+        return self.hood_name
+
+    class Meta:
+        ordering = ['-id']
+        
 class User(models.Model):
     first_name=models.CharField(max_length=50)
-    user_id=models.IntegerField(max_length=50)
+    user_id=models.IntegerField()
     email_address=models.CharField(max_length=50)
     neighborhood_id=models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.first_name
 
 class Business(models.Model):
     business_name=models.CharField(max_length=50)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     neighborhood_id=models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
     business_address=models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.business_name
