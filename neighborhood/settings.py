80c0f486d5af4d3febe451887ff591bc52c94c30
saudahabib <saudababs00@gmail.com>
2019-06-06 11:08:11 +0300

    include bootstrap and navigations

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jiraan',
-    'bootstrap4'
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
