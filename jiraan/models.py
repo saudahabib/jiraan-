814249c3395eb1d8c65b29cb7544f0ed3fd2ea32
saudahabib <saudababs00@gmail.com>
2019-06-06 11:37:31 +0300

    created classes in models.py

--- jiraan/models.py
+++ jiraan/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Neighborhood(models.Model):
+    hood_name=models.CharField(max_length=50)
+    hood_location = models.CharField(max_length=50)
+    hood_count=models.CharField(max_length=50)
+
+class User(models.Model):
+    first_name=models.CharField(max_length=50)
+    user_id=models.IntegerField(max_length=50)
+    email_address=models.CharField(max_length=50)
+    neighborhood_id=models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+class Business(models.Model):
+    business_name=models.CharField(max_length=50)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    neighborhood_id=models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    business_address=models.CharField(max_length=50)
