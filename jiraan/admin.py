be88f94d0229e97adc41438bc27bcf1dbce85458
saudahabib <saudababs00@gmail.com>
2019-06-06 18:16:45 +0300

    register models on admin page

--- jiraan/admin.py
+++ jiraan/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User, Business, Neighborhood
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(User)
